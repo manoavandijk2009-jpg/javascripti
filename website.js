a1e888bb56e09c21bf0be96d4ce2bf9b9aba8334
manoa <manoavandijk@outlook.com>
2026-02-13 09:40:15 +0100

    commit

---
+++ website.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function () {
+    console.log("De pagina is geladen!");
+});
+
+document.querySelector('#box1').addEventListener('click', function() {
+    console.log("Ooooh je mag niet klikken!");
+    document.querySelector('#box2').innerHTML = "<h1>Dit is nu</h1><p>de tekst in Box Twee.</p>";
+});
+document.querySelector('#box2').addEventListener('click', function() {
+    document.querySelector('#box1').innerHTML = "<h2>welkom wereld hallo mars.</h2>";
+    console.log("Ooooh je mag niet klikken!")});
+
+

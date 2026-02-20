cc0449478b39b767002b568fc2d80f6beed9744a
manoa <manoavandijk@outlook.com>
2026-02-20 09:16:10 +0100

    Added json

--- website.js
+++ website.js
@@ -10,4 +10,17 @@ document.querySelector('#box2').addEventListener('click', function() {
     document.querySelector('#box1').innerHTML = "<h2>welkom wereld hallo mars.</h2>";
     console.log("Ooooh je mag niet klikken!")});
 
+let gebruiker={
+    "naam": "manoa",
+    "leeftijd": 17,
+    "hobbys": [
+        "gamen",
+        "slapen",
+        "eten"
+    ],
+    "blijft laat op maar houdt wel van slaap": true,
 
+
+}
+document.querySelector('#box3').innerHTML = "<h2>burh</h2>"
+    console.log(gebruiker["naam"]);
